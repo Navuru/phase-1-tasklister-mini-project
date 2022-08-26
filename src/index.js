a40a63570498b2ffd65058e44c737b854984d4d6
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-08-26 07:06:44 +0300

    Complete assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener("submit",(e) =>{
+    e.preventDefault();
+    buildList(e.target.new_task_description.value);
+    form.reset()
+  })
+
 });
+
+function buildList(todo){
+  // console.log(todo);
+  let task = document.createElement("li")
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteTask)
+  btn.textContent = "x"
+  task.textContent = `${todo} `
+  task.appendChild(btn);
+  // console.log(task)
+  document.querySelector("#tasks").appendChild(task)
+
+}
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
